24820edf60e875b993c7e5314da14f6719f04e63
liadk07 <91607695+liad07@users.noreply.github.com>
2022-06-09 09:22:40 +0300

    Update main.js

--- main.js
+++ main.js
@@ -10,13 +10,12 @@
 
     function showPosition(position) {
         $.ajax({
-            url: "https://api.openweathermap.org/data/2.5/weather?lat="+position.coords.latitude+"&lon="+position.coords.longitude+"&appid=4fbee6f796d9742c10ccc097c5c76006&units=metric",
+            url: "https://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&appid=4fbee6f796d9742c10ccc097c5c76006&units=metric",
             jsonpCallback: "callback",
             dataType: "jsonp",
-            success: function( location ) {
-                $('#demo').html(location.main.temp);
+            success: function (location) {
+                $('#demo').html(Math.floor(location.main.temp));
             }
-        });
-        x.innerHTML = "Latitude: " + position.coords.latitude +
-            "<br>Longitude: " + position.coords.longitude;
+
+        })
     }
